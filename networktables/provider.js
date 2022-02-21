357f9ff6fa4dc6dd85421850c36b6be9d210ce6f
Amory Galili <amorygalili@gmail.com>
2022-02-21 01:00:49 -0500

    Added NetworkTables source provider

--- networktables/provider.js
+++ networktables/provider.js
@@ -1,3 +1,27 @@
 import NetworkTables from './networktables.js';
 const { SourceProvider } = require('@webbitjs/store');
 
+export default class NetworkTablesProvider extends SourceProvider {
+
+  constructor() {
+    super();
+    this.connect('localhost');
+    NetworkTables.addGlobalListener((key, value) => {
+      this.updateSource(key, value);
+    }, true);
+  }
+
+  connect(address) {
+    if (address) {
+      localStorage.networkTablesAddress = address === 'localhost' ? '127.0.0.1' : address;
+    }
+
+    if (localStorage.networkTablesAddress) {
+      NetworkTables.connect(localStorage.networkTablesAddress);
+    }
+  }
+
+  userUpdate(key, value) {
+    NetworkTables.setValue(key, value);
+  }
+}
