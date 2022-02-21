ba1d4a80172466d37f67c0cc0f6990f75045987f
Amory Galili <amorygalili@gmail.com>
2022-02-21 00:26:55 -0500

    Added function to set networktable values

--- networktables/networktables.js
+++ networktables/networktables.js
@@ -1,4 +1,18 @@
 const { NetworkTableInstance, EntryListenerFlags } = require('node-ntcore');
+const { NetworkTableType } = require('node-ntcore/dist/networktables/network-table-instance.js');
+
+function isArrayType(type) {
+  const arrayTypes = [
+    NetworkTableType.BOOLEAN_ARRAY, 
+    NetworkTableType.DOUBLE_ARRAY,
+    NetworkTableType.STRING_ARRAY
+  ];
+  return arrayTypes.includes(type);
+}
+
+function isEmptyArray(value) {
+  return value instanceof Array && value.length === 0;
+}
 
 class NetworkTables {
 
@@ -19,6 +33,25 @@ class NetworkTables {
     return this._getValueFromEntry(entry.getValue());
   }
 
+  setValue(key, value) {
+    const entry = this._ntInstance.getEntry(key);
+
+    const currentType = entry.getType();
+    const newValueType = this._getType(value);
+
+    if (currentType === NetworkTableType.UNASSIGNED) {
+      this._setEntryFromType(entry, value, newValueType);
+    } else {
+      const matchesType = (
+        (currentType === newValueType) 
+        || (isArrayType(currentType) && isEmptyArray(newValueType))
+      );
+      if (matchesType) {
+        this._setEntryFromType(entry, value, currentType);
+      }
+    }
+  }
+
   addGlobalListener(listener) {
     this._ntInstance.addEntryListener('', (key, entry, value) => {
       const valueFromEntry = this._getValueFromEntry(value);
@@ -26,6 +59,53 @@ class NetworkTables {
         listener(key, valueFromEntry);
       }
     }, EntryListenerFlags.UPDATE | EntryListenerFlags.NEW, EntryListenerFlags.IMMEDIATE);
+  }
+
+  _setEntryFromType(entry, value, type) {
+    switch(type) {
+      case NetworkTableType.DOUBLE:
+        entry.setDouble(value);
+        break;
+      case NetworkTableType.STRING:
+        entry.setString(value);
+        break;
+      case NetworkTableType.BOOLEAN:
+        entry.setBoolean(value);
+        break;
+      case NetworkTableType.DOUBLE_ARRAY:
+        entry.setDoubleArray(value);
+        break;
+      case NetworkTableType.STRING_ARRAY:
+        entry.setStringArray(value);
+        break;
+      case NetworkTableType.BOOLEAN_ARRAY:
+        entry.setBooleanArray(value);
+        break;
+    }
+  }
+
+  _getType(value) {
+    if (typeof value === 'number') {
+      return NetworkTableType.DOUBLE;
+    }
+    if (typeof value === 'string') {
+      return NetworkTableType.STRING;
+    }
+    if (typeof value === 'boolean') {
+      return NetworkTableType.BOOLEAN;
+    }
+    if (value instanceof Array) {
+      if (value.length === 0 || value.every(element => typeof element === 'number')) {
+        return NetworkTableType.DOUBLE_ARRAY;
+      }
+      if (value.every(element => typeof element === 'string')) {
+        return NetworkTableType.STRING_ARRAY;
+      }
+      if (value.every(element => typeof element === 'boolean')) {
+        return NetworkTableType.BOOLEAN_ARRAY;
+      }
+    }
+    return undefined;
   }
 
   _getValueFromEntry(value) {
