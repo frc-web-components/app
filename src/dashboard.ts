8c4032920abb0b97184c0080db28fc3e3d5d86b4
amorygalili <amorygalili@gmail.com>
2024-01-10 21:50:35 -0500

    Updated dashboard field images and package versions

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -4,21 +4,21 @@ import {
   addThemeRules,
   mountDashboard,
   setDefaultSourceProvider,
-  setAssetBasePath
+  setAssetBasePath,
 } from "@frc-web-components/app";
 import { Nt4Provider } from "@frc-web-components/fwc/source-providers";
 import { darkTheme } from "@frc-web-components/fwc/themes";
 import { dashboardElementConfigs } from "@frc-web-components/fwc";
 
-setAssetBasePath('http://localhost:18127/assets');
+setAssetBasePath("http://localhost:18127/assets");
 addThemeRules("dark", darkTheme);
 addSourceProvider("NetworkTables", new Nt4Provider());
 setDefaultSourceProvider("NetworkTables");
-addElements(dashboardElementConfigs as any);
+addElements(dashboardElementConfigs, "FRC");
 mountDashboard(document.body);
 
 import * as fwcApp from "@frc-web-components/app";
 (window as any).fwcApp = fwcApp;
 
-import * as lit from 'lit';
+import * as lit from "lit";
 (window as any).lit = lit;
